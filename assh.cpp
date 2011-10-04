5ce87110706ff0636ac1c2e1ec2bf715094bc9be
BEN ENGLISCH <EMAILBEN145@gmail.com>
2011-10-03 22:02:56 -0500

    hello();

--- assh.cpp
+++ assh.cpp
@@ -1,7 +1,18 @@
 #include "shell.h"
 
+void hello()
+{
+	printf( "\n"
+		    " ╔═║╔═╝╔═╝║ ║╔═╝║  ║ \n"
+		    " ╔═║══║══║╔═║╔═╝║  ║ \n"
+		    " ╝ ╝══╝══╝╝ ╝══╝══╝══\n"
+		    "   actionscript.shell\n"
+		    "           ? for help\n\n");
+}
+
 int main (int argc, char **argv)
 {
+	hello();
 	int code = run_shell(argc, argv);
 	return code;
 }
